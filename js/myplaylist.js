d888379af081ad48f161c2bbb4a62b6b2155a124
Dcsbarte <dcs_bate@hotmail.com>
2017-06-01 16:14:19 -0300

    ! 1 versão !

---
+++ js/myplaylist.js
@@ -0,0 +1,65 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01.mp3',
+        title:'o.n.i.f.c',
+        artist:'Wiz Khalifa',
+        rating:5,
+        buy:'#',
+        price:'1.99',
+        duration:'5:32',
+        cover:'images/album.jpg'
+    },
+
+    {
+        mp3:'mix/02.mp3',
+        title:'black yellow',
+        artist:'wiz khalifa',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:49',
+        cover:'images/album2.jpg'
+    },
+
+    {
+        mp3:'mix/03.mp3',
+        title:'Berner',
+        artist:'Wiz Khalifa e Berner',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'5:32',
+        cover:'images/album3.jpg'
+    },
+
+    {
+        mp3:'mix/04.mp3',
+        title:'Berner',
+        artist:'Wiz Khalifa',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:19',
+        cover:'images/album4.jpg'
+    },
+
+    {
+        mp3:'mix/05.mp3',
+        title:'snopp',
+        artist:'Wiz Khalifa e snopp',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:10',
+        cover:'images/album5.jpg'
+    }
+];
